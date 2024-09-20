b70c116ec3679bd83f273fc836739cbc4a7e6df7
Hunter Codexs <huntercodexs@gmail.com>
2024-09-20 08:32:30 -0300

    init: Intial commit on branch

---
+++ src/main/java/com/huntercodexs/javaspringbootcucumber/JavaSpringBootCucumberApplication.java
@@ -0,0 +1,13 @@
+package com.huntercodexs.javaspringbootcucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootCucumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huntercodexs/javaspringbootcucumber/JavaSpringBootCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huntercodexs.javaspringbootcucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
